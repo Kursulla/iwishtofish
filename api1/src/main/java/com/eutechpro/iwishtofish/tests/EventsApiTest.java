978f502d57b42c13226e4fe36e67a954d7946f38
Nebojsa Bozic <bozic85@gmail.com>
2015-08-16 19:44:12 +0200

    Setup jUnit for API module (api1)

---
+++ api1/src/main/java/com/eutechpro/iwishtofish/tests/EventsApiTest.java
@@ -0,0 +1,20 @@
+package com.eutechpro.iwishtofish.tests;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Kursulla on 16/08/15.
+ */
+public class EventsApiTest extends TestCase {
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testGetAllEvents() throws Exception {
+        assertFalse("Nije null", true);
+    }
+
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+}
