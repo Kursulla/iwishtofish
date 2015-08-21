487fdf7b16c17c6945e9b37254088b24d10ec59b
Nebojsa Bozic <bozic85@gmail.com>
2015-08-21 10:33:50 +0200

    Added "Dismiss" button to snack bar

--- app/src/main/java/com/iwishtofish/SnackBarControl.java
+++ app/src/main/java/com/iwishtofish/SnackBarControl.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.TextView;
 
 /**
@@ -22,11 +23,16 @@ public class SnackBarControl {
 
     private static Snackbar createSimpleSnackBar(String message, Activity activity) {
         CoordinatorLayout coordinatorLayout = (CoordinatorLayout) activity.findViewById(R.id.coordinator_layout);
-        Snackbar snackbar = Snackbar.make(coordinatorLayout, message, Snackbar.LENGTH_SHORT);
+        final Snackbar snackbar = Snackbar.make(coordinatorLayout, message, Snackbar.LENGTH_LONG);
         View snackBarView = snackbar.getView();
         TextView textView = (TextView) snackBarView.findViewById(android.support.design.R.id.snackbar_text);
-
-        snackbar.setActionTextColor(activity.getResources().getColor(R.color.primary_dark));
+        snackbar.setAction("Dismiss", new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                snackbar.dismiss();
+            }
+        });
+        snackbar.setActionTextColor(Color.WHITE);
         snackBarView.setBackgroundColor(activity.getResources().getColor(R.color.primary));
 
         textView.setTextColor(Color.WHITE);
