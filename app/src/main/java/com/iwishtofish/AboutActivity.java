d68699700985ef829303b541523c71308ac14147
Nebojsa Bozic <bozic85@gmail.com>
2015-08-24 11:00:27 +0200

    About fetch real App version and code

--- app/src/main/java/com/iwishtofish/AboutActivity.java
+++ app/src/main/java/com/iwishtofish/AboutActivity.java
@@ -5,6 +5,9 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.graphics.Palette;
 import android.support.v7.graphics.Palette.Swatch;
+import android.widget.TextView;
+
+import com.iwishtofish.utils.AppUtil;
 
 /**
  * Created by Kursulla on 23/08/15.
@@ -15,6 +18,9 @@ public class AboutActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        TextView version = (TextView)findViewById(R.id.app_version);
+        version.setText("Application version: "+ AppUtil.appVersion(this)+" ("+AppUtil.appCode(this)+")");
 
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.about_image);
         Palette palette = Palette.from(bitmap).generate();
